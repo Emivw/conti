8a0dc812e0fbebf06d37bd7f8ade57baf38ee063
Emi-vw <emile@itemate.com>
2024-05-27 08:23:51 +0200

    update pc return

--- components/FeatureCard.vue
+++ components/FeatureCard.vue
@@ -5,6 +5,7 @@
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Features Module</h5>
         </a>
+        <img src="../public/assets/Movie-theatre-camera.jpg" alt="image_theatre">
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <a href="/features" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Start Production
@@ -16,6 +17,7 @@
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Series Module</h5>
         </a>
+        <img src="../public/assets/Movie-theatre-camera.jpg" alt="image_theatre">
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <a href="/series" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Start Production
